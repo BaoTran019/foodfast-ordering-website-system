3585c42875a902492bf7186f4f741d5e6ab3ec93
Minnut225 <147785678+Minnut225@users.noreply.github.com>
2025-12-16 13:37:29 +0700

    Update OrderServiceImpl.java

--- backend/src/main/java/com/cnpm/Service/impl/OrderServiceImpl.java
+++ backend/src/main/java/com/cnpm/Service/impl/OrderServiceImpl.java
@@ -113,7 +113,7 @@ public class OrderServiceImpl implements OrderService {
         double totalPrice = 0;
         for (CartItem cartItem : cart.getCartItems()) {
             order.addOrderItems(cartItem.getProduct(), cartItem.getQuantity(), cartItem.getProduct().getPrice());
-            totalPrice += cartItem.getQuantity() * cartItem.getProduct().getPrice());
+            totalPrice += cartItem.getQuantity() * cartItem.getProduct().getPrice();
         }
         order.setStatus("Pending");
         order.setOrderDate(LocalDateTime.now());
